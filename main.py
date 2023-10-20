b354f072eb1bb469c3be673ecab248b580d2f26f
Bilal <belkhoul@uoguelph.ca>
2023-10-21 02:11:23 +0400

    Initialized all classes

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Turtle Crossing")
 screen.tracer(0)
+
+player = Player()
+cars = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    cars.create_car()
+    cars.move_cars()
+
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,20 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.color("white")
+        self.penup()
+        self.goto(x=-280,y=250)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+        self.level += 1
+
